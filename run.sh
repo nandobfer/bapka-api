4d76b4c30e1bf26f34e7d4284e292ad834b1e965
nandobfer <nandobfer@yahoo.com.br>
2022-09-17 19:06:08 -0300

    edited cert

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd /home/bapkasorvetes/cupons_webserver/bapka-api
-screen -m -d -S bapka-api python3 -m flask run --port=4000 --debugger --host="0.0.0.0" --cert=cupons_bapkasorvetes_com_br_da29f_1faa9_1668268697_1c36aca5cb2cb6eeae02f44c346bc2a3.crt --key=ssl.key
+screen -m -d -S bapka-api python3 -m flask run --port=4000 --debugger --host="0.0.0.0" --cert=../../ssl/certs/cupons_bapkasorvetes_com_br_da29f_1faa9_1668268697_1c36aca5cb2cb6eeae02f44c346bc2a3.crt --key=ssl.key
+
+# 04c7e485f4909d3d6c933577843f62c73976
