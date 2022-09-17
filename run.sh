7d2209f39363e0df372b80df2b44d04295020f62
nandobfer <nandobfer@yahoo.com.br>
2022-09-17 18:37:25 -0300

    fixed typo

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/bapkasorvetes/cupons_webserver/bapka-api
-screen -m -d -S bapka-api flask run --port=4000 --debugger --host="0.0.0.0" --cert=ssl.crt --key=ssl.key
+screen -m -d -S bapka-api python3 -m flask run --port=4000 --debugger --host="0.0.0.0" --cert=ssl.crt --key=ssl.key
